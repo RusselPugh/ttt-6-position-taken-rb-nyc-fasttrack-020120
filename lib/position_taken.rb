9038e00cf7aaf9a5d6a88a9ccfddf2c17f0e47ea
Russel Pugh <russ_scooby@yahoo.com>
2020-02-03 19:16:49 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -2,11 +2,9 @@
 board = Array.new(9, " " || "")
 
 def position_taken?(board, index)
-  if board[index] == " " || "" || nil
+  if board[index] == (" " || "" || nil)
     false
-    puts "Move is available"
   else board[index] == "X" || "O"
     true
-    puts "Move is taken, try again"
   end
 end
