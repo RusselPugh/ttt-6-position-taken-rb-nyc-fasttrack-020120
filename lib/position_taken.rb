7b65de6c91a5763b0d2d3698996dd78b2136ce5d
Russel Pugh <russ_scooby@yahoo.com>
2020-02-03 19:14:24 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -5,10 +5,8 @@ def position_taken?(board, index)
   if board[index] == " " || "" || nil
     false
     puts "Move is available"
-    elsif board[index] == "X" || "O"
+  else board[index] == "X" || "O"
     true
-    puts "Move is taken"
-  else
     puts "Move is taken, try again"
   end
 end
